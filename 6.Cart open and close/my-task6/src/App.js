4817a349b4c2062b5c2767a42ce315213d929bd8
Moiz <moizuddinr189@gmail.com>
2023-10-03 01:25:01 +0530

    States Handling

---
+++ 6.Cart open and close/my-task6/src/App.js
@@ -0,0 +1,28 @@
+import { Fragment, useState } from 'react'
+import Meals from './components/Meals/Meals';
+
+import Header from './components/Layout/Header'
+import Cart from './components/Cart/Cart';
+const App = () => {
+  const[cartIsShown,setCartIsShown]=useState(false);
+  
+  const showCartHandler = ()=>{
+     setCartIsShown(true);
+  };
+
+  const hideCartHandler = ()=>{
+     setCartIsShown(false);
+  };
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+     <Header onShowCart={showCartHandler}/>
+    <main>
+      <Meals/>
+    </main>
+    </Fragment>
+  )
+}
+
+export default App;
