8d59092a9e108ae7501a5e3c7b353cc0c0a26c66
Moiz <moizuddinr189@gmail.com>
2023-10-02 20:22:51 +0530

    Created a Modal and backdrop

---
+++ 5.Making a hard Coded Cart without any Functionality/my-task5/src/App.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react'
+import Meals from './components/Meals/Meals';
+
+import Header from './components/Layout/Header'
+import Cart from './components/Cart/Cart';
+const App = () => {
+  return (
+    <Fragment>
+      <Cart/>
+     <Header/>
+    <main>
+      <Meals/>
+    </main>
+    </Fragment>
+  )
+}
+
+export default App;
